5c8b8ca7bef77b0051fee7074b7201dcbf5c676e
dvdkrstndnls <karstenisgerman@yahoo.com>
2018-07-03 14:07:11 -0500

    basic header created

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Who can beat Trump in 2020?</h1>
+          <h3 className="App-title">(a memory game)</h3>
+          
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
