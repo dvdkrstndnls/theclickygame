bbdb71e18a43b10b7ed5417270eb90ec868cfa3c
dvdkrstndnls <karstenisgerman@yahoo.com>
2018-07-03 14:34:54 -0500

    copy editing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,7 +13,8 @@ class App extends Component {
           
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {/* To get started, edit <code>src/App.js</code> and save to reload. */}
+          This game is simple, click each face but only once!
         </p>
       </div>
     );
