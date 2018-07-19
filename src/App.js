82a28bcf87f1acb6e8938ff57746ceea7891fd2c
dvdkrstndnls <karstenisgerman@yahoo.com>
2018-07-19 14:57:14 -0500

    broke some things. consider revert

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
+import Title from "./components/Title/Title";
 import Card from "./components/Card/Card";
 import Wrapper from "./components/Wrapper";
 import candidates from "./candidates.json";
@@ -6,178 +9,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  /* setting this.state.candidates to the candidates json array */
+  state = {
+    candidates
+  };
+
+  removeFriend = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const candidates = this.state.candidates.filter(candidate => candidate.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({
+      candidates
+    });
+  };
+
+
+  // I think we will use this.shuffle instead of remove  :: https://vestride.github.io/Shuffle/shuffle-with-react
+  //Map over this.state.candidates and render a candidate component for each candidate object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">WHO CAN FLUSH AWAY TRONALD DUMP IN 2020?</h1>
-          <h3 className="App-subtitle">- a memory game -</h3>
-          <h4 className="App-score">Score: 0</h4>
-          
-        </header>
-        
-        <h5 className="App-intro">
-          {/* To get started, edit <code>src/App.js</code> and save to reload. */}
-          This game is simple, click each face but <b>only</b> once!
-        </h5>
+      <Wrapper>
+      <Title> Candidates </Title>  
+      {this.state.candidates.map(candidate => (
+          <Card 
+            removeFriend = {this.removeFriend}
+          id = {candidate.id}
+          image = {
+            candidate.image
+          }
+          />
+        ))
+      }
+      </Wrapper>
 
-       
-      
+     
 
-        {/* <div class="wrapper">
-          
-            <span class="card">
-              <div class="img-container">
-                <img src="images/berniesanders.jpg" alt="Bernie Sanders"></img>
-              </div>
-                <a class="style_prevu_kit"></a> 
-            </span>
+      // <footer className = "App-footer" >
+      // <h3 className = "App-subtitle" > & copy; 2018 by < a href = "https://dvdkrstndnls.github.io/Portfolio/" > DKD < /a> || <a href="https:/ / github.com / dvdkrstndnls / theclickygame ">Peek at the code</a> on GitHub</h3>
 
-            <span class="card">
-              <div class="img-container">
-                <img src="images/chrismurphy.jpg" alt="Chris Murphy"></img>
-              </div>
-                <a class="style_prevu_kit"></a> 
-            </span>
+      // </footer> <p className = "App-intro" > 
+      // { /* To get started, edit <code>src/App.js</code> and save to reload. */ } </p> 
 
-            <span class="card">
-              <div class="img-container">
-                <img src="images/corybooker.jpg" alt="Cory Booker"></img>
-              </div>
-                <a class="style_prevu_kit"></a> 
-            </span>
-
-            <span class="card">
-              <div class="img-container">
-                <img src="images/devalpatrick.jpeg" alt="Deval Patrick"></img>
-              </div>
-                <a class="style_prevu_kit"></a> 
-            </span>
-
-            <span class="card">
-              <div class="img-container">
-                <img src="images/elizabethwarren.jpg" alt="Elizabeth Warren"></img>
-              </div>
-                <a class="style_prevu_kit"></a> 
-            </span>
-
-            <span class="card">
-              <div class="img-container">
-                <img src="images/joebiden.jpg" alt="Joe Biden"></img>
-              </div>
-                <a class="style_prevu_kit"></a> 
-            </span>
-
-            <span class="card">
-              <div class="img-container">
-                <img src="images/kamalaharris.jpg" alt="Kamala Harris"></img>
-              </div>
-                <a class="style_prevu_kit"></a> 
-            </span>
-
-            <span class="card">
-              <div class="img-container">
-                <img src="images/kirstengillibrand.jpg" alt="Kirsten Gillibrand"></img>
-              </div>
-                <a class="style_prevu_kit"></a> 
-            </span>
-
-            <span class="card">
-              <div class="img-container">
-                <img src="images/markcuban.jpeg" alt="Mark Cuban"></img>
-              </div>
-                <a class="style_prevu_kit"></a> 
-            </span>
-
-            <span class="card">
-              <div class="img-container">
-                <img src="images/oprahwinfrey.jpg" alt="Oprah Winfrey"></img>
-              </div>
-                <a class="style_prevu_kit"></a> 
-            </span>
-
-            <span class="card">
-              <div class="img-container">
-                <img src="images/sherrodbrown.jpg" alt="Sherrod Brown"></img>
-              </div>
-                <a class="style_prevu_kit"></a> 
-            </span>
-
-            <span class="card">
-              <div class="img-container">
-                <img src="images/terrymcauliffe.jpeg" alt="Terry McAuliffe"></img>
-              </div>
-                <a class="style_prevu_kit"></a> 
-            </span>
-
-            
-            
-        </div> */}
-
-
-{/* // const App = () => ( might need this again */}
-  <Wrapper> 
-    {/* <h1 className="title"></h1> */}
-    <Card
-      image={candidates[0].image}
-    />
-    <Card
-      image={candidates[1].image}
-    />
-    <Card
-      image={candidates[2].image}
-    />
-    <Card
-      image={candidates[3].image}
-    />
-    <Card
-      image={candidates[4].image}
-    />
-    <Card
-      image={candidates[5].image}
-    />
-    <Card
-      image={candidates[6].image}
-    />
-    <Card
-      image={candidates[7].image}
-    />
-    <Card
-      image={candidates[8].image}
-    />
-    <Card
-      image={candidates[9].image}
-    />
-    <Card
-      image={candidates[10].image}
-    />
-    <Card
-      image={candidates[11].image}
-    />
-  </Wrapper>
-{/* ); */}
-
-        <footer className="App-footer">
-          <h3 className="App-subtitle">&copy; 2018 by <a href="https://dvdkrstndnls.github.io/Portfolio/">DKD</a> || <a href="https://github.com/dvdkrstndnls/theclickygame">Peek at the code</a> on GitHub</h3>
-          
-        </footer>
-        <p className="App-intro">
-          {/* To get started, edit <code>src/App.js</code> and save to reload. */}
-        </p>
-        {/* const FriendCard = props => (
-  <div className="card">
-    <div className="img-container">
-      <img alt={props.name} src={props.image} />
-    </div>
-    <div className="content">
-      
-    </div>
-    <span className="remove">𝘅</span>
-  </div>
-); */}
-
-      </div>
+      // {/* // </div> */}
     );
   }
 }
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { Component } from 'react';
 import "./Title.css";
 
-const Title = props => <h1 className="title">{props.children}</h1>;
+// const Title = props => <h1 className="title">{props.children}</h1>;
+
+class Title extends Component {
+    render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            {/* <img src={logo} className="App-logo" alt="logo" /> */}
+            <h1 className="App-title">WHO CAN FLUSH AWAY TRONALD DUMP IN 2020?</h1>
+            <h3 className="App-subtitle">- a memory game -</h3>
+            <h4 className="App-score">Score: 0</h4>
+            
+          </header>
+          
+          <h5 className="App-intro">
+            {/* To get started, edit <code>src/App.js</code> and save to reload. */}
+            This game is simple, click each face but <b>only</b> once!
+          </h5>
+        </div>
+              );
+  }
+}
 
 export default Title;
